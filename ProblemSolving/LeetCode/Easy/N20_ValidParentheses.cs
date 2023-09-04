7497452176f6d443fc99c6b7812399022975d80b
yk170901 <yk170901@gmail.com>
2023-09-04 13:22:50 +0900

    Make folders and tidy up codes of TextRPG

---
+++ ProblemSolving/LeetCode/Easy/N20_ValidParentheses.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    internal class N20_ValidParentheses : IProblem
+    {
+        public int Index => 20;
+
+        public string Name => "Valid Parentheses";
+
+        public string Link => "https://leetcode.com/problems/valid-parentheses/";
+
+        public DateTime SolvedDate => new DateTime(2023,09,04);
+
+        public void BestCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestCode()
+        {
+            ValidParenthesis("[]{]()");
+        }
+
+        public bool ValidParenthesis(string s)
+        {
+            char[] startParen = { '(', '{', '['};
+            char[] endss = { ')', '}', ']'};
+
+
+            string start = "{[(";
+            string end = "}])";
+            char cBefore = '\0';
+            foreach (char c in s)
+            {
+                //Array.Find(startParen)
+
+
+                //if(end.Any(x => x.Equals(cBefore)) && Array.Find(startParen, x => x.Equals(cBefore)))
+                //{
+
+                //}
+
+                if (end.Any(x => x.Equals(cBefore)) || cBefore.Equals('\0'))
+                {
+                    cBefore = c;
+                }
+            }
+
+            return true;
+        }
+    }
+}
