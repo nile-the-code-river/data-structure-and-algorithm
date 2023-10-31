f0b81859316d5d19e3fb8843bb19c9c6774a41e0
yk170901 <yk170901@gmail.com>
2023-10-31 20:09:41 +0900

    Add LeetCode - N70_ClimbingStairs

---
+++ ProblemSolving/LeetCode/Easy/N70_ClimbingStairs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    internal class N70_ClimbingStairs : IProblem
+    {
+        public int Index => 70;
+
+        public string Name => "Climbing Stairs";
+
+        public string Description => "Given a number of stairs, return the number of distinct ways of climbing chairs WHEN you can climb the stairs 1 step or 2 steps per time.";
+
+        public string Link => "https://leetcode.com/problems/climbing-stairs/description/";
+
+        public DateTime SolvedDate => new (2023,10,31);
+
+        public void TestCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ClimbingStairs(int num)
+        {
+            int one = 1;
+            int two = 1;
+            int temp = 0;
+
+            for(int i = 0; i < num; i++)
+            {
+                temp = one;
+                one += two;
+                two = temp;
+            }
+
+            return one;
+        }
+    }
+}
---
+++ ProblemSolving/ProblemSolving/Playground.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving
+{
+    internal class Playground
+    {
+        public void Run()
+        {
+            TestDelegate test = Method1;
+            test += Method2;
+
+            foreach (var del in test.GetInvocationList())
+            {
+                var res = del.DynamicInvoke();
+                Console.WriteLine(del.Method);
+                Console.WriteLine(res);
+            }
+
+            Console.Read();
+        }
+
+        private delegate bool TestDelegate();
+
+        private bool Method1()
+        {
+            return true;
+        }
+        private bool Method2()
+        {
+            return false;
+        }
+    }
+}
