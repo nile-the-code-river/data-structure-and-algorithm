ffd5921b7ab299333e03bcd2c8c618d972e4562e
yk170901 <yk170901@gmail.com>
2023-12-26 16:48:46 +0900

    Add DataStructures & Algorithms Solution

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Algorithms
+{
+    internal class Program
+    {
+        // Call methods to test within this method
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        // Call methods to test within this method
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
