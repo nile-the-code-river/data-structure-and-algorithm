9b425841bd37dd0b01444e32c096846acef841cd
yk170901 <yk170901@gmail.com>
2023-12-26 17:15:13 +0900

    Add Template LeetCode - N94_BinaryTreeInorderTraversal

---
+++ ProblemSolving/LeetCode/Easy/N94_BinaryTreeInorderTraversal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Easy
+{
+    internal class N94_BinaryTreeInorderTraversal : IProblem
+    {
+        public int Index => 94;
+
+        public string Name => "Binary Tree Inorder Traversal";
+
+        public string Description => throw new NotImplementedException();
+
+        public string Link => "https://leetcode.com/problems/binary-tree-inorder-traversal";
+
+        public DateTime SolvedDate => throw new NotImplementedException();
+
+        public void TestCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
