f77023e6864a22ba6766ba10462b2bd8cd537325
Yeseul Kim <yk170901@gmail.com>
2024-02-04 17:33:43 +0900

    implement hashtable

--- _DataStructure/HashTable/00_HashTable.cs
+++ _DataStructure/HashTable/00_HashTable.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace _DataStructure.HashTable
 {
-    internal class _00_HashTable
+    internal class _00_HashTable<TKey, TValue>
     {
+        private List<KeyValuePair<TKey, TValue>> _storedPairs = new();
+
+        public void Push(TKey key, TValue value)
+        {
+            _storedPairs.Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        public TValue Get(TKey key)
+        {
+            foreach(var pair in _storedPairs)
+            {
+                if(EqualityComparer<TKey>.Default.Equals(pair.Key, key))
+                    return pair.Value;
+            }
+
+            return default;
+        }
     }
 }
--- _DataStructure/Program.cs
+++ _DataStructure/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _DataStructure
+﻿using _DataStructure.HashTable;
+
+namespace _DataStructure
 {
     class Program
     {
@@ -11,18 +13,25 @@
 
             //Console.WriteLine(test);
 
-            Array._02_MergeSortedArrays arrays = new Array._02_MergeSortedArrays();
+            //Array._02_MergeSortedArrays arrays = new Array._02_MergeSortedArrays();
 
-            int[] a = { 0, 3, 7, 9 };
-            int[] b = { 1, 2, 4, 10 };
+            //int[] a = { 0, 3, 7, 9 };
+            //int[] b = { 1, 2, 4, 10 };
 
-            int[] result = arrays.MergeSortedArrays(a, b);
+            //int[] result = arrays.MergeSortedArrays(a, b);
 
-            foreach (var item in result.ToList())
-            {
-                Console.Write(item + " ");
+            //foreach (var item in result.ToList())
+            //{
+            //    Console.Write(item + " ");
 
-            }
+            //}
+
+            HashTable._00_HashTable<string, string> hashTable = new HashTable._00_HashTable<string, string>();
+
+            hashTable.Push("01","test1");
+            hashTable.Push("02","test2");
+
+            Console.WriteLine(hashTable.Get("01"));
 
             return 0;
         }
